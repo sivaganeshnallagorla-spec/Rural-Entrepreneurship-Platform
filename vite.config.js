8ded92b52358d57f23a17c78f0981f5a6d68e621
sivaganeshnallagorla-spec <sivaganeshnallagorla@gmail.com>
2025-11-03 07:35:53 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Rural-Entrepreneurship-Platform/",  // ✅ Required for GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
@@ -10,4 +11,3 @@ export default defineConfig({
     host: true
   }
 })
-
